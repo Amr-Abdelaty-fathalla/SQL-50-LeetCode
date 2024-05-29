859099e927d23113e3944489ac7a6aab381698d4
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2024-05-29 13:01:23 +0300

    [584] [Time Beats: 69.07%] - LeetPush

--- 584-Find-Customer-Referee.sql
+++ 584-Find-Customer-Referee.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-SELECT name
+-- SELECT name
+-- FROM Customer
+-- WHERE referee_id != 2 or referee_id IS NULL;
+
+# Another Solution Using IFNULL()
+SELECT name 
 FROM Customer
-WHERE referee_id != 2 or referee_id IS NULL;
+WHERE IFNULL(referee_id,0) <> 2;
