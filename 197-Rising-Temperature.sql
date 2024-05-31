84fec634bb239840ad505cab7bd31a7ba16fa15c
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2024-06-01 02:02:13 +0300

    [197] [Time Beats: 98.66%] - LeetPush

--- 197-Rising-Temperature.sql
+++ 197-Rising-Temperature.sql
@@ -2,5 +2,6 @@
 SELECT w_tod.id AS id
 FROM Weather w_yes
 INNER JOIN Weather w_tod
-ON w_yes.recordDate + INTERVAL 1 DAY = w_tod.recordDate 
+ON w_yes.recordDate = w_tod.recordDate - INTERVAL 1 DAY
 WHERE w_tod.temperature > w_yes.temperature;
+
