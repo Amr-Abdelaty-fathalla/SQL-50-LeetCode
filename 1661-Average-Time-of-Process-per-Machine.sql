02293cd403d8ae66676b7fb007e3f657530433d0
Amr Abdelaty <90145088+Amr-Abdelaty-fathalla@users.noreply.github.com>
2024-06-11 02:12:10 +0300

    [1661] [Time Beats: 99.08%] - LeetPush

--- 1661-Average-Time-of-Process-per-Machine.sql
+++ 1661-Average-Time-of-Process-per-Machine.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
+-- SELECT a.machine_id,
+-- ROUND(AVG(b.timestamp - a.timestamp),3) AS processing_time
+-- FROM Activity a, Activity b
+-- WHERE a.machine_id = b.machine_id AND a.process_id = b.process_id AND a.activity_type = 'start' AND b.activity_type = 'end'
+-- GROUP BY a.machine_id;
+
+-- Another Solution Usin Self JOIN
 SELECT a.machine_id,
 ROUND(AVG(b.timestamp - a.timestamp),3) AS processing_time
-FROM Activity a, Activity b
-WHERE a.machine_id = b.machine_id AND a.process_id = b.process_id AND a.activity_type = 'start' AND b.activity_type = 'end'
+FROM Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id AND a.process_id = b.process_id AND a.activity_type = 'start' AND b.activity_type = 'end'
 GROUP BY a.machine_id;
